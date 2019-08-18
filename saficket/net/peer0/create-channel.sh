862b97c068b1e17168119cf813d77172fc913c1b
김동준 <skdltm357@naver.com>
2019-08-19 05:00:23 +0900

    cryptogen network peer0 init

---
+++ saficket/net/peer0/create-channel.sh
@@ -0,0 +1,16 @@
+FABRIC_PATH=/home/dongjun/p1034_swing/saficket/net/peer0
+
+echo $FABRIC_PATH
+
+
+
+CORE_PEER_LOCALMSPID="Org0MSP" \
+
+CORE_PEER_TLS_ROOTCERT_FILE=$FABRIC_PATH/crypto-config/peerOrganizations/org0/peers/peer0.org0/tls/ca.crt \
+
+CORE_PEER_MSPCONFIGPATH=$FABRIC_PATH/crypto-config/peerOrganizations/org0/users/Admin@org0/msp \
+
+CORE_PEER_ADDRESS=peer0:7051 \
+
+/home/dongjun/p1034_swing/saficket/src/github.com/hyperledger/fabric/.build/bin/peer channel create -o orderer:7050 -c ch1 -f ch1.tx
+
