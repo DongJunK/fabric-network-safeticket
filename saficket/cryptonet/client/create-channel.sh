9afffa4cdd529a924f9cd6a02fa127089b11fa8d
김동준 <skdltm357@naver.com>
2019-08-19 06:20:39 +0900

    create create-channel.sh in client node

---
+++ saficket/cryptonet/client/create-channel.sh
@@ -0,0 +1,15 @@
+FABRIC_PATH=/home/dongjun/p1034_swing/saficket/cryptonet/$HOSTNAME
+
+echo $FABRIC_PATH
+
+
+
+CORE_PEER_LOCALMSPID="Org0MSP" \
+
+CORE_PEER_TLS_ROOTCERT_FILE=$FABRIC_PATH/crypto-config/peerOrganizations/org0/peers/peer0.org0/tls/ca.crt \
+
+CORE_PEER_MSPCONFIGPATH=$FABRIC_PATH/crypto-config/peerOrganizations/org0/users/Admin@org0/msp \
+
+CORE_PEER_ADDRESS=peer0:7051 \
+
+peer channel create -o orderer:7050 -c ch1 -f ch1.tx
